04239cd1265a836797642c701b10db99d93cb503
tuomohy <tuomo.hyttinen@kapsi.fi>
2013-10-07 21:26:47 +0300

    Add heading to index-page  (example commit for git-session).

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,6 +5,7 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>CMDB-testi</h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
 
