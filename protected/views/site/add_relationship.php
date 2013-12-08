0f3603cb361b29fd80cac241b33e7a8247abd258
Aarne U <aarneus@gmail.com>
2013-12-08 21:06:04 +0200

    Listing items according to type in the relationships adding screen

--- protected/views/site/add_relationship.php
+++ protected/views/site/add_relationship.php
@@ -40,17 +40,20 @@ $this->breadcrumbs = array(
     <div class="row">
         <div class="span6">
             
-    <!--
-            
+    
+ 
+            <!-- javascript for changing the visibility of items by their type -->
             <select id="inputType">
-                <option>Kaikki</option>
-                <option>Käyttöjärjestelmä</option>
-                <option>Palvelin</option>
-                <option>Sovellus</option>
-                <option>Työasema</option>
-                <option>Tulostin</option>
+                
+                <?php
+                $types = Type::getAll();
+                foreach ($types as $type):
+                ?>
+                <option><?php echo $type->name; ?></option>
+                <?php endforeach; ?>
+                
             </select>
-    -->
+    
         </div>
         <div class="span6">
             
@@ -72,53 +75,60 @@ $this->breadcrumbs = array(
         
         <div class="span12">
           
-           
+           <!-- Relationships are separated into divs by item type -->
+            <?php foreach ($types as $type): ?>
+            <div id="<?php echo $type->name; ?>" style="display:none;">
             
-            <table class="table table-striped">
-                
-                
-                <thead>
-                    <tr>
-                        <th>Depends on</th>
-                        <th>Dependency to</th>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Type</th>
-                    </tr>
-                </thead>
-                
-               
-                
-                
-                <tbody>
-                    
-                    
-                    <?php foreach ($relationships as $i => $relationship): ?>
+                <table class="table table-striped">
 
-                    <tr>
-                        <td>
-                        <?php echo $form->checkBox($relationship, "[$i]depends_on"); ?>
-                        </td>
-                        <td>
-                        <?php echo $form->checkBox($relationship, "[$i]dependency_to"); ?>
-                        </td>
-                        <?php
-                        $item = Item::model()->findByPk($relationship->item_id);
-                        $type_name = $item->type->name;
+
+                    <thead>
+                        <tr>
+                            <th>Depends on</th>
+                            <th>Dependency to</th>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Type</th>
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+
+                        <?php foreach ($relationships as $i => $relationship): 
+                        
+                        $relationship_item = Item::model()->findByPk($relationship->item_id);
+                        if ($relationship_item->type_id == $type->id):
                         ?>
-                        <td> <?php echo $item->id; ?> </td>
-                        <td> <?php echo $item->name; ?> </td>
-                        <td> <?php echo $type_name; ?> </td>
-                   </tr>
-                   
-                   <?php endforeach; ?>
-                    
-                    
-                   
-                </tbody>
-                
-                
-            </table>
+
+                        <tr>
+                            <td>
+                            <?php echo $form->checkBox($relationship, "[$i]depends_on"); ?>
+                            </td>
+                            <td>
+                            <?php echo $form->checkBox($relationship, "[$i]dependency_to"); ?>
+                            </td>
+                            <?php
+                            $item = Item::model()->findByPk($relationship->item_id);
+                            $type_name = $item->type->name;
+                            ?>
+                            <td> <?php echo $item->id; ?> </td>
+                            <td> <?php echo $item->name; ?> </td>
+                            <td> <?php echo $type_name; ?> </td>
+                       </tr>
+
+                       <?php endif; ?>
+                       <?php endforeach; ?>
+
+
+
+                    </tbody>
+
+
+                </table>
+            </div>
+            
+            <?php endforeach; ?>
             
             <br/>
             <div class="controls">
@@ -135,3 +145,26 @@ $this->breadcrumbs = array(
     
     <?php $this->endWidget(); ?>
 </div>
+
+
+
+<script type="text/javascript">
+    //this is jQuery's equivalent to document.ready -function
+    //it's called when the page is loaded
+    $(function() {
+    
+    // Sets the default visible relationship div
+    var currentViewedType = "#" + $("#inputType").val();
+    $(currentViewedType).css({"display":"inline"});
+    
+    //Changes the visibility of the form <div>:s so those selected from the type list are shown
+    $("#inputType").change(function(e) {
+        $(currentViewedType).css({"display":"none"});
+        currentViewedType = "#" + $(this).val();
+        $(currentViewedType).css({"display":"inline"});
+    });
+    
+    
+    });
+    
+</script>
