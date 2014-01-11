e047ae251df33ccf4cc33262510e3cf7b2ec7d3c
Aarne U <aarneus@gmail.com>
2014-01-11 18:09:11 +0200

    Unit testing; setup test folder and basic test

--- protected/tests/unit/BasicTest.php
+++ protected/tests/unit/BasicTest.php
@@ -5,25 +5,18 @@
  *
  * The most basic test possible for testing the unit testing process
  */
-class BasicTest extends CDbTestCase {
+class BasicTest extends CTestCase {
    
-    // Contains the active records used for this test. The fixtures are reset each time the test is run
-    public $fixtures = array(
-        'items' => 'Item',
-        'types' => 'Type'
-        );
+ 
     
     
     
     // All test functions are formatted testExample
-    // This adds a type and then finds it, testing the saving and retrieving of types
     public function testType() {
         $type = new Type;
         $type->name = 'Test-type';
-        $this->assertTrue($type->save(false));
+        $this->assertTrue($type->name == 'Test-type');
         
-        $type = Type::model()->findByPk($type->id);
-        $this->assertTrue($type instanceof Type);
     }
 
     
