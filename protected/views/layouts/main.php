d27975e26f7acc6e8e5d943685ab1100bdaf7096
Aarne U <aarneus@gmail.com>
2014-02-22 17:54:05 +0200

    Login components hidden from view

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -37,11 +37,13 @@ Yii::app()->clientScript->registerCoreScript('jquery');
                             )
                         ));
                         ?>
+                        <!-- Unused
                         <form class="navbar-form pull-right">
                             <input class="span2" type="text" placeholder="Email">
                             <input class="span2" type="password" placeholder="Password">
                             <button type="submit" class="btn">Sign in</button>
                         </form>
+                        -->
                     </div>
                 </div>
             </div>
